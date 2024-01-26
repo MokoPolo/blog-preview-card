9620452200bcbb2074561c1057476e0bc63b5db9
= <=>
2024-01-26 16:40:33 -0500

    Update styles and fonts

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,21 +9,21 @@ import React from "react";
 const Card: React.FC = () => {
   return (
     <div
-      className="card flex flex-col items-start justify-center bg-white rounded-lg w-[350px] p-4"
-      style={{ boxShadow: "5px 5px 1px black" }}
+      className="card flex flex-col items-start justify-center bg-white rounded-xl w-[350px] p-4 border border-black"
+      style={{ boxShadow: "8px 8px 1px black" }}
     >
       <img className="rounded-lg" src="./images/illustration-article.svg"></img>
       <p>Learning </p>
-      <p>Published 21 Dec 2023</p>
+      <h1>Published 21 Dec 2023</h1>
       <p className="font-extrabold text-lg">HTML & CSS foundations</p>
-      <p>
+      <p className="py-3">
         These languages are the backbone of every website, defining structure,
         content, and presentation.{" "}
       </p>
-      <div className="flex">
-        <img src="./images/image-avatar.webp"></img>
+      <div className="flex my-4">
+        <img src="./images/image-avatar.webp" className="w-8 h-8" />
         <div className="flex items-center">
-          <span className="">Greg Hooper</span>
+          <span className="ml-3 font-bold">Greg Hooper</span>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
-    extend: {
-      boxShadow: {
-        custom: "10 10px 5px 0px rgba(0,0,0,0.75)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
